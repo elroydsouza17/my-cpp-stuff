48beb413245953c937a58bab36b8b50e5f9ba1cb
elroydsouza <elroydsouza17@gmail.com>
2026-05-21 21:17:49 +0530

    added new pattern programs

---
+++ patterns/pattern3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void printPattern(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << (i + j + 1) % 2 << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main()
+{
+    printPattern(7);
+}
+
